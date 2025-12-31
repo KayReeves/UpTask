d27b9b4eee68a8bf8b848059f09181272d1f3940
Karan Kapali <karankapali507@gmail.com>
2025-12-31 09:48:54 +0545

    cors config fixed

--- UpTask_Backend/UpTask/src/main/java/com/bca4th/UpTask/config/CorsConfig.java
+++ UpTask_Backend/UpTask/src/main/java/com/bca4th/UpTask/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
@@ -13,7 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        config.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5500"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
